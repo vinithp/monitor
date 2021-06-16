9ad353f022008f4c57eb8dc5b200b13c286a17eb
vinithp <vinithp100@Gmail.com>
2021-06-16 21:09:21 +0530

    new

--- newsub.sh
+++ newsub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 print_usage(){
     printf '%s\n' " NEWSUB - MONITOR NEW SUBDOMAIN "
-    printf '\n%s\n%s\n%s\n' '-h: help' '-i: install dependence' '-m: provid domain to monitor new subdomains'
+    printf '\n%s\n%s\n%s\n%s\n' '-h: help' '-b: provid domain for initial run ' '-i: install dependence' '-m: provid domain to monitor new subdomains'
     printf '\n%s\n' 'useage: ./newsub.sh -m example.com'
 }
 
@@ -9,25 +9,28 @@ install(){
     mkdir -p ~/bug/newsub/
     mkdir -p ~/tools/my/
     cp -r ~/newsub ~/tools/my/
-    touch ~/bug/newsub/new_$domain ~/bug/newsub/all_$domain
     mkdir -p ~/.config/amass/jk/
     cp ~/newsub/config/config.ini ~/.config/amass/jk/config.ini
     sudo apt install tmux
     go version || (wget https://dl.google.com/go/go1.16.5.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz)
-    source ~/.bashrc
-    go get -v github.com/OWASP/Amass/v3/...
+    amass -version || go get -v github.com/OWASP/Amass/v3/...
     echo -e 'tmuxr(){\ntmux attach-session -t $1\n}' >> ~/.bashrc
     echo -e 'tmuxs(){\ntmux new -s $1\n}' >> ~/.bashrc
     echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
     echo 'export PATH=$PATH:~/go/bin' >> ~/.bashrc
-    echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc 
+    echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
     echo 'export PATH=$PATH:/usr/local/bin/' >> ~/.bashrc
+}
+brun(){
+    touch ~/bug/newsub/new_$domain ~/bug/newsub/all_$domain
+    amass enum -config ~/.config/amass/jk/config.ini -passive -d $domain -nocolor | tee ~/bug/newsub/new_$domain
+
 }
 
 newsub(){
     while true 
     do
-        amass enum -config ~/.config/amass/jk/config.ini -passive -d $domain -nocolor | tee ~/bug/newsub/new_$domain 
+        amass enum -config ~/.config/amass/jk/config.ini -passive -d $domain -nocolor | tee ~/bug/newsub/new_$domain
         comm -1 -3 <(sort ~/bug/newsub/new_$domain) <(sort ~/bug/newsub/all_$domain) | (grep . || echo no) | tee -a ~/bug/newsub/deleted | sed 's/$/ deleted/' | ~/tools/my/newsub/./tnotify.sh
         comm -2 -3 <(sort ~/bug/newsub/new_$domain) <(sort ~/bug/newsub/all_$domain) | (grep . || exit 0) | sed 's/$/ added/' | ~/tools/my/newsub/./tnotify.sh
         cp ~/bug/newsub/new_$domain ~/bug/newsub/all_$domain
@@ -37,11 +40,13 @@ newsub(){
 }
 
 [ $# -gt 0 ] || (print_usage && exit 0)
-while getopts 'him:' flag; do
+while getopts 'hib:m:' flag; do
     case $flag in
         h)print_usage ;;
         i)install ;;
+        b)domain=${OPTARG} && brun ;;
         m)domain=${OPTARG} && newsub ;;
         *)print_usage ;;
     esac
 done
+
